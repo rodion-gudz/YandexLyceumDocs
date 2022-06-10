1fab0764a1292348aee6be6c128778704825240a
lav <me@lavn.ml>
2022-06-10 15:16:50 +0300

    add `onboarding-test` type

--- generator/stuff.py
+++ generator/stuff.py
@@ -4,4 +4,5 @@ sections_types = {
     "additional": "Дополнительные задачи",
     "individual-work": "Самостоятельная работа",
     "control-work": "Контрольная работа",
+    "onboarding-test": "Вступительный тест"
 }
